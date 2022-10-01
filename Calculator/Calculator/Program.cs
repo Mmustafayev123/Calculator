5b6e17977c10c9d408d50bab9407bfbee1b371dd
MahammadMustafayev <tu7ekdm78@code.edu.az>
2022-10-01 04:36:52 -0700

    evo

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,16 +10,13 @@ namespace Calculator
             int num2 = 0;
 
             Console.WriteLine("Calculator");
-            Console.WriteLine("==================");
+            Console.WriteLine("=================");
 
-            Console.WriteLine("Birinci ededi secin:");
+            Console.WriteLine("Birinci ededi daxil edin:");
             num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("===============");
-
-            Console.WriteLine("Ikinci ededi secin:");
+            Console.WriteLine("Ikinci ededi daxil edin:");
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("================");
 
             int choise;
 
@@ -28,32 +25,35 @@ namespace Calculator
                 Console.WriteLine("Emeliyyat secin:");
                 Console.WriteLine("\t1-Toplama");
                 Console.WriteLine("\t2-Vurma");
-                Console.WriteLine("\t3-Cixma");
-                Console.WriteLine("\t4-Bolme");
+                Console.WriteLine("\t3-Bolme");
+                Console.WriteLine("\t4-Cixma");
                 Console.WriteLine("\t5-Cixis");
                 choise = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("============================");
 
                 switch (choise)
                 {
                     case 1:
-                        Console.WriteLine($"Cavab: {num1} + {num2} = " + (num1+num2));
+                        Console.WriteLine($"Cavab: {num1} + {num2}=" + (num1+num2) );
                         break;
                     case 2:
-                        Console.WriteLine($"Cavab: {num1} * {num2} = " +(num1*num2));
+                        Console.WriteLine($"Cavab: {num1} * {num2}=" + (num1 * num2));
                         break;
                     case 3:
-                        Console.WriteLine($"Cavab: {num1} - {num2} = " + (num1 - num2));
+                        Console.WriteLine($"Cavab: {num1} / {num2}=" + (num1 / num2));
                         break;
                     case 4:
-                        Console.WriteLine($"Cavab: {num1} / {num2} = " + (num1 / num2));
+                        Console.WriteLine($"Cavab: {num1} - {num2}=" + (num1 - num2));
                         break;
                     case 5:
-                        Console.WriteLine("Saqolun");
+                        Console.WriteLine("Good Bye!");
                         break;
                     default:
                         break;
                 }
+
             } while (choise != 5);
+
         }
     }
 }
